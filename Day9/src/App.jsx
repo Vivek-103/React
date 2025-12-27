cdc639af634a9bbb8d2e1ff7882799efbcfb0c17
Vivek-103 <viveksrivastava1003@gmail.com>
2025-12-27 15:13:57 +0530

    Form validation and 2 way binding

---
+++ Day9/src/App.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react'
+
+const App = () => {
+
+  const [name, setName] = useState('')
+
+  const submitHandler =(e) =>{
+    e.preventDefault();
+    console.log("form Submitted by" , name)
+    setName('')
+  }
+
+  return (
+    <div>
+      <form onSubmit={(e)=>{
+        submitHandler(e)
+      }}>
+        <input type='text' placeholder='Enter your Name' 
+        value={name}
+        onChange={(e)=>{
+          setName(e.target.value);
+        }}
+         />
+        <button>Submit</button>
+      </form>
+    </div>
+  )
+}
+
+export default App
