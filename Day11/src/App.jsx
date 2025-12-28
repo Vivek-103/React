52a65f0b1b0142bc6f3adcb44af763c9165e1d93
Vivek-103 <viveksrivastava1003@gmail.com>
2025-12-28 16:17:44 +0530

    Api and its usage

---
+++ Day11/src/App.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import axios from 'axios'
+
+const App = () => {
+
+  // async function getData(){
+  //   console.log("Data fetched");
+  //   const response =  await fetch('https://jsonplaceholder.typicode.com/todos/1')
+
+  //   console.log(response)
+  // }
+
+  // const getData = async() => {
+  //   const response = await fetch('https://jsonplaceholder.typicode.com/todos/1')
+
+  //   const data = await response.json();
+
+  //   console.log(data);
+  // }
+
+
+  // NOW USING AXIOS
+   const getData = async()=>{
+    const response = await axios.get('https://jsonplaceholder.typicode.com/todos/1')
+
+    console.log(response.data);
+
+    console.log('Data fetched using axios')
+  }
+
+  return (
+    <div>
+      <button onClick={getData}>
+        Get Data
+      </button>
+    </div>
+  )
+}
+
+export default App
